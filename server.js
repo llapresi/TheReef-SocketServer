f6e2c1cda930e72d8970c48b1a6aee224b1398b9
Luke Lapresi (RIT Student) <lxl6996@ad.rit.edu>
2019-01-28 14:12:50 -0500

    server should broadcast stuff now

--- server.js
+++ server.js
@@ -27,5 +27,13 @@ wss.on('connection', socket => {
         }
       })
     }
+
+    if(parsedData.type === 'rotate') {
+      wss.clients.forEach((client) => {
+        if(client.readyState === WebSocket.OPEN) {
+          client.send(data);
+        }
+      })
+    }
   });
 });
