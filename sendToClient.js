b328502ba9bc01534772573dfd57f36edff133c4
Luke Lapresi (RIT Student) <lxl6996@ad.rit.edu>
2019-02-28 13:14:29 -0500

    We need both the wss parameter AND a require('ws') to get sendToClient.js to work

--- sendToClient.js
+++ sendToClient.js
@@ -1,3 +1,5 @@
+const WebSocket = require('ws');
+
 module.exports = (wss, clientID, data) => {
   wss.clients.forEach((client) => {
     if(client.readyState === WebSocket.OPEN && client.clientID === clientID) {
