bba98e724b40ce701f19dc3601ca96d6117fd77b
Luke Lapresi (RIT Student) <lxl6996@ad.rit.edu>
2019-02-28 13:26:41 -0500

    seperated socket.on('connection') into it's own file

---
+++ socket-handlers/onMessage.js
@@ -0,0 +1,27 @@
+const UnityClient = require('./unityClient');
+const SendToClient = require('./sendToClient');
+
+module.exports = (wss, socket) => {
+  socket.on('message', data => {
+    // Parse the data to JSON and attach the server assigned client ID
+    let parsedData = JSON.parse(data);
+    parsedData.id = socket.clientID;
+
+    // Switch based on our message type
+    switch(parsedData.type) {
+      case "isUnity":
+        // Create UnityClient if we get an "isUnity" message
+        UnityClient.createUnitySocket(socket);
+        break;
+      case "rotate":
+      case "fire":
+        // Send rotate and fire messages to the Unity instance
+        UnityClient.send(parsedData);
+        break;
+      case "targetInfo":
+        // Send returned target info messages to only specified user in parsedData (provided by Unity client)
+        SendToClient(wss, parsedData.userID, data);
+        break;
+    }
+  });
+};
