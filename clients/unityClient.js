752173efa9b5962030ff6ad6170fb0c3543871b3
Luke Lapresi <llapresi@me.com>
2019-03-30 21:35:37 -0400

    fixed line endings and crap, but honesly I'm just gonna tell eslint to ignore them next commit

--- clients/unityClient.js
+++ clients/unityClient.js
@@ -1,27 +1,26 @@
 const WebSocket = require('ws');
 
-let unitySocket = undefined;
+let unitySocket;
 
-const canSendMessage = () => {
-  return(unitySocket !== undefined && unitySocket.readyState === WebSocket.OPEN);
-}
+const canSendMessage = () => unitySocket !== undefined && unitySocket.readyState === WebSocket.OPEN;
 
 // On message with "isUnity" property set to true we store our Unity client
 exports.createUnitySocket = (socket) => {
-  socket.isUnity = true;
-  unitySocket = socket;
+  const incomingSocket = socket;
+  incomingSocket.isUnity = true;
+  unitySocket = incomingSocket;
   console.log('Unity instance has connected');
 };
 
 // Clear our unitySocket variable if the current unity socket disconnects
 exports.checkForDisconnection = (socket) => {
-  if(socket === unitySocket) {
+  if (socket === unitySocket) {
     unitySocket = undefined;
   }
-}
+};
 
 exports.send = (msg) => {
-  if(canSendMessage()) {
+  if (canSendMessage()) {
     unitySocket.send(JSON.stringify(msg));
-  };
+  }
 };
